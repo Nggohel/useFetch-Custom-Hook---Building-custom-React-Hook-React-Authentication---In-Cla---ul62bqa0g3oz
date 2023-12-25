c76a88508819fc66b5f9e56061838d6d21a5f9ef
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 18:47:46 +0530

    add useFetch Hook

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import useFetch from '../components/useFetch';
+// ./pages/index.js
+import React from "react";
+import useFetch from "../components/useFetch";
 
 function Home() {
-  const { data, loading, error } = useFetch("https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?limit=10&page=1");
+  const { data, loading, error } = useFetch(
+    "https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?limit=10&page=1"
+  );
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -10,13 +13,7 @@ function Home() {
   return (
     <div>
       <h1>Products</h1>
-      {/* {document.write(data)} */}
-      <ul>
-      {data && data.map(item => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-
-      </ul>
+      <ul>{data && data.map((item) => <li key={item.id}>{item.title}</li>)}</ul>
     </div>
   );
 }
